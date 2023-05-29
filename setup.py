1ae23b0628e5badb70b00a7891bdd87a2926fa9e
Shoxrux Yuldashov <shoxrux.yuldashov@yandex.ru>
2023-05-30 00:06:20 +0100

    Первое обновление
    - Добавлен теоретический материал темы
    - Добавлены задания по теме вместе с шаблонами для решения задач
    - Добавлен файл `.gitignore`
    - Добавлены файлы настроек проекта (`setup.cfg`, `setup.py`)

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="topic_3",
+    version="1.0.0",
+    author="Shoxrux Yuldashov",
+    description="3. Переменные и объекты",
+)
---
+++ tasks/task_7.py
@@ -0,0 +1,8 @@
+working_hours = ...  # здесь Ваш код
+
+hourly_rate = ...  # здесь Ваш код
+
+salary = hourly_rate * working_hours
+
+print(...)  # допишите код
+print(...)  # допишите код

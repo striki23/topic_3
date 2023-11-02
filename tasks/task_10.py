84bb1224f2ed2383a07e1bf02434bca215184f90
Дарья Демидова <striki06@mail.ru>
2023-11-02 09:16:17 +0300

    done

--- tasks/task_10.py
+++ tasks/task_10.py
@@ -1,12 +1,17 @@
-name = ...  # здесь Ваш код
-surname = ...  # здесь Ваш код
-age = ...  # здесь Ваш код
+name = input('Введите ваше имя: ')  # здесь Ваш код
+surname = input('Введите вашу фамилию: ')  # здесь Ваш код
+age = int(input('Введите ваш возраст: '))  # здесь Ваш код
 
 # Определите типы данных и идентификаторы в памяти
 # Дайте смысловые имена переменным
-
+type_name = type(name)
+type_surname = type(surname)
+type_age = type(age)
+id_name = id(name)
+id_surname = id(surname)
+id_age = id(age)
 
 # вывод типа и идентификатора каждой переменной
-print(...)  # допишите код
-print(...)  # допишите код
-print(...)  # допишите код
+print(f'Тип имени: {type_name}, ID в памяти: {id_name}')  # допишите код
+print(f'Тип фамилии: {type_surname}, ID в памяти: {id_surname}')  # допишите код
+print(f'Тип возраста: {type_age}, ID в памяти: {id_age}')  # допишите код
--- tasks/task_7.py
+++ tasks/task_7.py
@@ -1,8 +1,12 @@
-hourly_rate = ...  # здесь Ваш код
+hourly_rate = int(input('Введите почасовую ставку: '))  # здесь Ваш код
 
-working_hours = ...  # здесь Ваш код
+WORKING_HOURS_PER_DAY = 8
+WORKING_DAYS_PER_MONTH = 22
 
-salary = ...  # здесь Ваш код
+working_hours = WORKING_DAYS_PER_MONTH*\
+                WORKING_HOURS_PER_DAY  # здесь Ваш код
 
-print(...)  # допишите код
-print(...)  # допишите код
+salary = hourly_rate*working_hours # здесь Ваш код
+
+print(f'Вы проработали {working_hours} часов')  # допишите код
+print(f'Ваша зарплата в месяц: {salary} рублей')  # допишите код
